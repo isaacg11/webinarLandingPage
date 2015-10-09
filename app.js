1bc20dd454df5bdcb2a7e28114e52b4e0bc45b0f
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-09 13:01:54 -0700

    object creation works

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 Stamplay.init("registertemplate");
-var user = new Stamplay.User().Model;
+
     document.getElementById("signup-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var registrationData = { 
-    			email: email, 
-    			password: password 
-    			};
+   var email = document.getElementById("email").value;
 
-    user.signup(registrationData).then(function(){
-        alert("Success!");
-    });
+   var Obj = {address:email};
+
+    var emailInstance = new Stamplay.Cobject('obj').Model;
+   emailInstance.set('address', email);
+   emailInstance.save().then(function(){
+    alert('Check your email!');
+   });
 });
+
+
+
+
+    //USER SIGN-UP
+
+    // var email = document.getElementById("email").value;
+    // var password = document.getElementById("password").value;
+    // var registrationData = { 
+    //          email: email, 
+    //          password: password 
+    //          };
+
+    // user.signup(registrationData).then(function(){
+    //     alert("Success!");
